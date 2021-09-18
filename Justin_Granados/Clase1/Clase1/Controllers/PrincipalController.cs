a67c1907e7f0f5196cac7cc409b2f10fd00271f0
Justin Granados Campos <justingranados34@gmail.com>
2021-09-17 20:46:41 -0600

    Trabajo en clase 1
    Clase 1: Se inicia proyecto en .Net Core, se trabajan con Controladores y vistas (se usa bootstrap)

---
+++ Justin_Granados/Clase1/Clase1/Controllers/PrincipalController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clase1.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
---
+++ Justin_Granados/Clase1/Clase1/Controllers/VentaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clase1.Controllers
+{
+    public class VentaController : Controller
+    {
+        public IActionResult Ventas()
+        {
+            return View();
+        }
+
+        public IActionResult RegistroVenta()
+        {
+            return View();
+        }
+    }
+}
