a0b8759046254970bca20e131c3494968c827464
Aldox09 <alexdr719@gmail.com>
2021-09-22 21:10:18 -0600

    Trabajo En Clase1

---
+++ Aplicacion de ejemplo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aplicacion_de_ejemplo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # progravanzavn6-9
-
 ## Crear un branch con su nombre y subir el trabajo en clase diario y tareas etc
 
-##Prueba1
+##Prueba 1
