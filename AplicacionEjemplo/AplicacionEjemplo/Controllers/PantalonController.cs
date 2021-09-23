90adac828233f76152ff7427ed77b036c0457e3a
angelo267 <55897123+angelo267@users.noreply.github.com>
2021-09-23 13:18:25 -0600

    Primer ejercicio

---
+++ AplicacionEjemplo/AplicacionEjemplo/Controllers/PantalonController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicacionEjemplo.Controllers
+{
+    public class PantalonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
