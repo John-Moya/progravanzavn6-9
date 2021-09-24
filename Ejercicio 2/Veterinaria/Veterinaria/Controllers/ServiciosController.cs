e3dd7145b12235df2d6ffaf4ee7dcd6674b1d315
karus96 <30331090+karus96@users.noreply.github.com>
2021-09-24 16:38:42 -0600

    Ejercicio 2

---
+++ Ejercicio 2/Veterinaria/Veterinaria/Controllers/ServiciosController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        public IActionResult Servicios()
+        {
+            return View();
+        }
+        // GET: Servicios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Servicios/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Servicios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servicios/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Servicios/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Servicios/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Servicios/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Servicios/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
