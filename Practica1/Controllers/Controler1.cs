6726dab5df68689cfb7c29dd3abbf40a1606c393
sandoval <nsandoval30860@ufide.ac.cr>
2021-09-24 12:18:07 -0600

    Trabajo en Clase Practica 1

---
+++ Practica1/Controllers/Controler1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1.Controllers
+{
+    public class Controler1 : Controller
+    {
+        // GET: Controler1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bienvenida()
+        {
+            return View();
+        }
+
+        public ActionResult Informacion()
+        {
+            return View();
+        }
+
+        // GET: Controler1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Controler1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Controler1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Controler1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Controler1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Controler1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Controler1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Practica1/Controllers/Controller4.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1.Controllers
+{
+    public class Controller4 : Controller
+    {
+        // GET: Controller4
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult estadisticas()
+        {
+            return View();
+        }
+
+        public ActionResult procesos()
+        {
+            return View();
+        }
+
+        // GET: Controller4/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Controller4/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Controller4/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Controller4/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Controller4/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Controller4/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Controller4/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
