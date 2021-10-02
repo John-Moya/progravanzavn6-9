4eb3b1032d65923501f14b1186bb5ef7f1893bf5
LAPTOP-M7L06JTO\Lenovo Ryzen 7 <rwilson90288@ufide.ac.cr>
2021-10-02 07:09:50 -0600

    Scaffold actividad en clase

---
+++ Zoologico/Zoologico/Data/ZoologicoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Zoologico.Models;
+
+namespace Zoologico.Data
+{
+    public class ZoologicoContext : DbContext
+    {
+        public ZoologicoContext (DbContextOptions<ZoologicoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Zoologico.Models.Historial> Historial { get; set; }
+    }
+}
---
+++ Zoologico/Zoologico/Models/Historial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoologico.Models
+{
+    public class Historial
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Aves { get; set; }
+
+        public string Mamiferos { get; set; }
+
+        public string Peces { get; set; }
+
+        public string Reptiles { get; set; }
+    }
+}
