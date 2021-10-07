743a87753b434163dedac88d9085a94f4a1e3157
Aldox09 <alexdr719@gmail.com>
2021-10-06 20:39:34 -0600

    Trabajo en clase

---
+++ Trabajos en Clase/Autenticacion/Controllers/ArticulosController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Autenticacion.Data;
+using Autenticacion.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Autenticacion
+{
+    [Authorize]
+    public class ArticulosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticulosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Articulos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Articulos.ToListAsync());
+        }
+
+        // GET: Articulos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articulos = await _context.Articulos
+                .FirstOrDefaultAsync(m => m.codigo == id);
+            if (articulos == null)
+            {
+                return NotFound();
+            }
+
+            return View(articulos);
+        }
+
+        // GET: Articulos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articulos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("codigo,tipo,marca,color,precio")] Articulos articulos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(articulos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articulos);
+        }
+
+        // GET: Articulos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articulos = await _context.Articulos.FindAsync(id);
+            if (articulos == null)
+            {
+                return NotFound();
+            }
+            return View(articulos);
+        }
+
+        // POST: Articulos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("codigo,tipo,marca,color,precio")] Articulos articulos)
+        {
+            if (id != articulos.codigo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(articulos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticulosExists(articulos.codigo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articulos);
+        }
+
+        // GET: Articulos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articulos = await _context.Articulos
+                .FirstOrDefaultAsync(m => m.codigo == id);
+            if (articulos == null)
+            {
+                return NotFound();
+            }
+
+            return View(articulos);
+        }
+
+        // POST: Articulos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var articulos = await _context.Articulos.FindAsync(id);
+            _context.Articulos.Remove(articulos);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticulosExists(int id)
+        {
+            return _context.Articulos.Any(e => e.codigo == id);
+        }
+
+
+        public IActionResult Articulos()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Trabajos en Clase/Autenticacion/Models/Articulos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autenticacion.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int codigo { get; set; }
+
+        public string tipo { get; set; }
+
+        public string marca { get; set; }
+
+        public string color { get; set; }
+
+        public string precio { get; set; }
+
+    }
+}
