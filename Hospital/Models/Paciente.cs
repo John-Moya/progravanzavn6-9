182eaf47f545f19a5e4f0c75a96e8f53dea98a50
unknown <dcoto50722@ufide.ac.cr>
2021-10-11 01:34:04 -0600

    Ejercicio 2 Semana 2

---
+++ Hospital/Models/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.Models
+{
+    public class Paciente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Condicion { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
